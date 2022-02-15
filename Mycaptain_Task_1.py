ae01d30a6c8e5c5ee7751e608261676ef43ddefc
Aswani677 <99744286+Aswani677@users.noreply.github.com>
2022-02-15 21:19:43 +0530

    Add files via upload

---
+++ Mycaptain_Task_1.py
@@ -0,0 +1,5 @@
+Radius = float(input(" Input the radius of the circle : "))
+# Area = pi*radius**2
+# pi = 3.14
+cal_area = float(3.14*Radius**2)
+print(" The area of the circle with radius",Radius,"is :",cal_area)
---
+++ Mycaptain_Task_2.py
@@ -0,0 +1,5 @@
+a = input("Input the filename : " )
+# splitted the string by using "."
+b = a.split('.')
+c  = b[-1] # this will print the last element of list
+print("The extension of the files is :",c)
